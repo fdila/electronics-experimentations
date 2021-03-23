21f8a87d834445dfba17c5ec41442e1cccc9f8f2
Federica Di Lauro <federicadilauro1998@gmail.com>
2021-03-23 11:40:28 +0100

    i parametri sono belli

--- Exp04/testUART.m
+++ Exp04/testUART.m
@@ -1,29 +1,20 @@
 function data = testUART(size, type)
 
+function readSerial(src, ~)
+    data = read(src,size,type);
+end
+
 device = serialport("COM4",9600);
 flush(device);
+data = [];
 configureCallback(device,"byte",20,@readSerial);
 write(device,10,"uint8");
 
-while ( numel(device.UserData) < 10)
+while (numel(data) < 10)
     pause(0.01);
 end
 
-data = device.UserData;
-
-function readSerial(src, ~)
-    if (strcmp(type,'uint16'))
-        dim = 2;
-    elseif (strcmp(type, 'uint32'))
-        dim = 4;
-    elseif (strcmp(type, 'uint8'))
-        dim = 1;
-    end
-    data = read(src,size*dim,type);
-    src.UserData = data;
-    
-    disp("ciao");
-end
+delete(device);
 
 end
 
