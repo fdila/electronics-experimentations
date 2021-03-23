014d0bcbba3bcd2829488d6b26fff83453a8dc17
Federica Di Lauro <federicadilauro1998@gmail.com>
2021-03-23 11:28:33 +0100

    matlab nuovo

--- Exp04/readSerial.m
+++
@@ -1,4 +0,0 @@
-function data = readSerial(src,evt)
-    data = read(src,10,"uint16");
-    disp(data);
-end
--- Exp04/testUART.m
+++ Exp04/testUART.m
@@ -1,11 +1,29 @@
-function testUART()
+function data = testUART(size, type)
 
 device = serialport("COM4",9600);
 flush(device);
 configureCallback(device,"byte",20,@readSerial);
 write(device,10,"uint8");
 
-while (true);
+while ( numel(device.UserData) < 10)
+    pause(0.01);
 end
+
+data = device.UserData;
+
+function readSerial(src, ~)
+    if (strcmp(type,'uint16'))
+        dim = 2;
+    elseif (strcmp(type, 'uint32'))
+        dim = 4;
+    elseif (strcmp(type, 'uint8'))
+        dim = 1;
+    end
+    data = read(src,size*dim,type);
+    src.UserData = data;
+    
+    disp("ciao");
+end
+
 end
 
