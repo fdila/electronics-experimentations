4e7ee9ab0844ede687838dd67782a43e3f8dcb3d
Federica Di Lauro <federicadilauro1998@gmail.com>
2021-05-24 12:10:09 +0200

    delete duplicated matlab code

--- Exp09/plotUART.m
+++
@@ -1,5 +0,0 @@
-function plotUART(data)
-    index = data(2001);
-    new_data = [data(index:2000), data(1:(index-1))];
-    plot(new_data);
-end
--- Exp09/uart_multiple_data.m
+++
@@ -1,7 +0,0 @@
-data = [];
-for i = 1:100
-    new_data = testUART(2001, 'uint16');
-    data = [data new_data(1:2000)];
-end
-
-histogram(data)
